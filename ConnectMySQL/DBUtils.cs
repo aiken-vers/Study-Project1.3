82762d871c06715b2759ccfe55bd2768958eddcd
Александр Зайцев <dalexator@gmail.com>
2020-08-12 11:58:53 +0500

    Добавьте файлы проекта.

---
+++ ConnectMySQL/DBUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConnectMySQL
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "bankdata";
+            string username = "root";
+            string password = "123456Qq";
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
---
+++ ConnectMySQL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConnectMySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQL.Connect();
+            Listener.StartListen();
+            while (Console.ReadLine() != "exit") { }
+        }
+    }
+}
